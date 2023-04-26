2f6b8c5b411c6f7b806b260bfe7959e01192bf63
Ezgi Deniz Çakır <114353663+denizcakr@users.noreply.github.com>
2023-04-26 13:56:15 +0000

    Update ft_split.c

--- ExamRank2/q4/ft_split.c
+++ ExamRank2/q4/ft_split.c
@@ -37,3 +37,64 @@ char **ft_split(char *str)
     tab[j] = 0;
     return(tab);
 }
+
+
+// 2. yol
+
+
+
+// #include <unistd.h>
+// #include <stdlib.h>
+
+// void ft_putstr(char *str)
+// {
+//     int i = 0;
+//     while(str[i])
+//         write(1, &str[i++], 1);
+// }
+
+// int word_count(char *str)
+// {
+//     int i = 0;
+//     int count = 0;
+//     while(str[i] && str[i] <= 32)
+//         i++;
+//     while(str[i])
+//     {
+//         while(str[i] && !(str[i] <= 32))
+//             i++;
+//         count++;
+//         while(str[i] &&  str[i] <= 32)
+//             i++;
+//     }
+//     return (count);
+// }
+
+// char **ft_split(char *str)
+// {
+//     int i = 0;
+//     int j = 0;
+//     int x = 0;
+//     int u = 0;
+//     char **res = (char **)malloc(sizeof(char *) * (word_count(str) + 1));
+
+//     while(str[i] && str[i] <= 32)
+//         i++;
+//     while(str[i])
+//     {
+//         j = 0, u= 0;
+//         while(str[i + u] && !(str[i + u] <= 32))
+//             u++;
+//         res[x] = (char *)malloc(sizeof(char) * (u + 1));
+//         while(str[i] && !(str[i] <=32))
+//         {
+//             res[x][j++] = str[i++];
+//         }
+//         res[x][j] = '\0';
+//         x++;
+//         while (str[i] && str[i] <=32)
+//             i++;       
+//     }
+//     res[x] = NULL;
+//     return(res);
+// }
