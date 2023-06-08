6bf2de0404c3c47f80efd719b3d145e507278eb1
Ezgi Deniz Çakır <114353663+denizcakr@users.noreply.github.com>
2023-06-08 15:08:44 +0000

    Update rev_wstr.c

--- ExamRank2/q4/rev_wstr.c
+++ ExamRank2/q4/rev_wstr.c
@@ -1,78 +1,30 @@
 
-#include <unistd.h>
-#include <stdlib.h>
-
-void ft_putstr(char *str)
-{
-    int i = 0;
-    while(str[i])
-        write(1, &str[i++], 1);
-}
-
-int word_count(char *str)
-{
-    int i = 0;
-    int count = 0;
-    while(str[i] && str[i] <= 32)
-        i++;
-    while(str[i])
-    {
-        while(str[i] && !(str[i] <= 32))
-            i++;
-        count++;
-        while(str[i] &&  str[i] <= 32)
-            i++;
-    }
-    return (count);
-}
-
-char **ft_split(char *str)
-{
-    int i = 0;
-    int j = 0;
-    int x = 0;
-    int u = 0;
-    char **res = (char **)malloc(sizeof(char *) * (word_count(str) + 1));
-
-    while(str[i] && str[i] <= 32)
-        i++;
-    while(str[i])
-    {
-        j = 0, u= 0;
-        while(str[i + u] && !(str[i + u] <= 32))
-            u++;
-        res[x] = (char *)malloc(sizeof(char) * (u + 1));
-        while(str[i] && !(str[i] <=32))
-        {
-            res[x][j++] = str[i++];
-        }
-        res[x][j] = '\0';
-        x++;
-        while (str[i] && str[i] <=32)
-            i++;       
-    }
-    res[x] = NULL;
-    return(res);
-}
+#include "unistd.h"
 
 int main(int ac, char **av)
 {
-    int i;
-    char **tab;
+    int i = 0;
+    int start;
+    int end;
+    int flag;
     if(ac == 2)
     {
-        i     = 0;
-        tab = ft_split(av[1]);
-        while (tab[i])      
+        while(av[1][i])
             i++;
-        i--;
-        while (i >= 0)
+        while(i >= 0)
         {
-            ft_putstr(tab[i]);
-            if(i > 0)
-                write(1, " ", 1);
-            i--;
+            while(av[1][i] <= 32)
+                i--;
+            end = i;
+            while(av[1][i] > 32)
+                i--;
+            start = i+1;
+            flag = start;
+            while(start <= end)
+                write(1, &av[1][start++], 1);
+            if(flag != 0)
+                write(1, " " ,1);
         }
-        write(1, "\n", 1);    
     }
+    write(1, "\n", 1);
 }
