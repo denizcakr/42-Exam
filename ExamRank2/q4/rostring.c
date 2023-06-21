f528eac9220c9e396eb71b1b23e941441d66c377
Ezgi Deniz Çakır <114353663+denizcakr@users.noreply.github.com>
2023-06-21 17:50:54 +0000

    Update rostring.c

--- ExamRank2/q4/rostring.c
+++ ExamRank2/q4/rostring.c
@@ -35,3 +35,67 @@ int main(int ac, char **av)
     }
     write(1, "\n", 1);
 }
+
+#include "stdlib.h"
+
+// char    **ft_split(char *str)
+// {
+//     int i = 0;
+//     int j = 0;
+//     int k = 0;
+//     char **tab;
+
+//     tab = (char **)malloc(sizeof(**tab) * 9999);
+
+//     while(str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+//         i++;
+//     while(str[i])
+//     {
+//         if(str[i] > 32)
+//         {
+//             k = 0;
+//             tab[j] = (char *)malloc(sizeof(char) * 9999);
+//             while(str[i] > 32)
+//             {
+//                 tab[j][k] = str[i];
+//                 i++;
+//                 k++;
+//             }
+//             tab[j][k] = '\0';
+//             j++;
+//         }
+//         else
+//             i++;
+//     }
+//     tab[j] = 0;
+//     return tab;
+// }
+
+// #include "unistd.h"
+// int main(int ac, char **av)
+// {
+//     if(ac > 1)
+//     {
+//         char **res = ft_split(av[1]);
+//         int n = 1;
+//         int i = 0;
+//         while(res[n])
+//         {
+//             i = 0;
+//             while(res[n][i])
+//             {
+//                 write(1, &res[n][i], 1);
+//                 i++;
+//             }
+//             write(1, " ", 1);
+//             n++;
+//         }
+//         i = 0;
+//         while(res[0][i])
+//         {
+//             write(1, &res[0][i], 1);
+//             i++;
+//         }
+//     }
+//     write(1, "\n", 1);
+// }
